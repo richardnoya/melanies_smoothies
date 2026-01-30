c8eae5ba0698337a6caad6c4f9f0b8c003bd5205
Richard Noya <richardnoya@icloud.com>
2026-01-30 14:07:06 +0100

    Update streamlit_app.py
    Smootiefroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -34,3 +35,6 @@ if (ingredients_list):
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
